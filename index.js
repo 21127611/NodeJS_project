bcd1bd53c792879cd16e1c68fb49f2382cbc8c62
21127611 <huyhoang1511216@gmail.com>
2024-01-02 16:46:09 +0700

    v1.0

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var app = express()
 //Define request response in root URL (/)  
 app.get('/', function (req, res) {  
  res.send('Hello World!')  
+ console.log('Hello World')
 })
 
 //Launch listening server on port 8081  
